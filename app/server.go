70bb03e8eead47f0a56c9889a7e4aa0c65d9b38d
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-06-12 15:42:41 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"strings"
 )
 
 var (
@@ -17,15 +20,19 @@ func main() {
 }
 
 func start() {
-	l, err := net.Listen("tcp", *listen)
+	listener, err := net.Listen("tcp", *listen)
 	if err != nil {
 		fmt.Println("\033[31mFailed to bind to port 6379\n\033[0m")
 		return
 	}
-	defer l.Close()
+	//defer l.Close()
 
+	createConnection(listener)
+}
+
+func createConnection(listener net.Listener) {
 	for {
-		conn, err := l.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("\033[31mError accepting connection: \033[0m", err.Error())
 			return
@@ -35,20 +42,46 @@ func start() {
 }
 
 func handleConnection(conn net.Conn) {
-	buf := make([]byte, 128)
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
 	for {
 		_, err := conn.Read(buf)
-		if err != nil {
+		if errors.Is(err, io.EOF) {
+			fmt.Println("Redis client connection closed")
+			break
+		} else if err != nil {
 			fmt.Println("\033[31mError reading input\033[0m", err.Error())
-			conn.Close()
 			return
 		}
 
-		_, err = conn.Write([]byte("+PONG\r\n"))
-		if err != nil {
-			fmt.Println("\033[31mError writing data\033[0m", err.Error())
-			conn.Close()
-			return
+		parts := strings.Split(string(buf), "\r\n")
+		fmt.Printf("Parts: %s\n", parts)
+
+		switch instruction := strings.ToUpper(parts[2]); instruction {
+		case "PING":
+			_, err = conn.Write([]byte("+PONG\r\n"))
+			if err != nil {
+				fmt.Println("\033[31mError writing data\033[0m", err.Error())
+				return
+			}
+		case "ECHO":
+			var echo string
+			if len(parts) == 6 {
+				echo = fmt.Sprintf("+%s\r\n", parts[4])
+			} else {
+				echo = "+(error) ERR wrong number of arguments for command\r\n"
+			}
+			_, err := conn.Write([]byte(echo))
+			if err != nil {
+				fmt.Println("\033[31mError writing data\033[0m", err.Error())
+				return
+			}
+		default:
+			fmt.Printf("%s: command not found\n", instruction)
+			errMessage := fmt.Sprintf("+COMMAND NOT RECOGNISED: %s.\r\n", instruction)
+			_, _ = conn.Write([]byte(errMessage))
 		}
+
 	}
 }
