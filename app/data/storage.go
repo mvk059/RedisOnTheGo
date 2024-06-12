177c6f8a4016e4dac3312803b539fd6506c18fff
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-06-12 21:45:21 +0530

    Refactor storage to add interface

--- app/data/storage.go
+++ app/data/storage.go
@@ -2,19 +2,24 @@ package data
 
 import "errors"
 
+type StorageHelper interface {
+	Set(key string, value string)
+	Get(key string) (string, error)
+}
+
 type Storage struct {
 	Data map[string]string
 }
 
-func NewStorage() Storage {
-	return Storage{Data: make(map[string]string)}
+func NewStorage() *Storage {
+	return &Storage{Data: make(map[string]string)}
 }
 
-func (s Storage) Set(key string, value string) {
+func (s *Storage) Set(key string, value string) {
 	s.Data[key] = value
 }
 
-func (s Storage) Get(key string) (string, error) {
+func (s *Storage) Get(key string) (string, error) {
 	value, ok := s.Data[key]
 	if !ok {
 		return "", errors.New("key not found")
